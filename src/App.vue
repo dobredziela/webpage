19c4612962cab83a69b1dad3bd3fe156feefdc52
Tomasz Kumięga <tomasz.kumiega@piotrskarga.pl>
2021-04-19 23:47:41 +0200

    about section

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <template>
-    <div>
-        <the-navbar />
-        <the-hero />
-    </div>
+	<div class="debug-screens">
+		<the-navbar />
+		<the-hero />
+		<the-main-container>
+			<the-about-section />
+		</the-main-container>
+	</div>
 </template>
 
 <script>
-import TheNavbar from './components/TheNavbar.vue'
-import TheHero from './components/TheHero.vue'
-
-export default {
-    name: 'App',
-    components: {
-        TheHero,
-        TheNavbar
-    }
-}
+	import TheAboutSection from './components/Page/TheAboutSection.vue'
+	import TheHero from './components/Page/TheHero.vue'
+	import TheMainContainer from './components/Page/TheMainContainer.vue'
+	import TheNavbar from './components/Page/TheNavbar.vue'
+	export default {
+		name: 'App',
+		components: {
+			TheHero,
+			TheNavbar,
+			TheMainContainer,
+			TheAboutSection
+		}
+	}
 </script>
 
 <style></style>
---
+++ src/components/Page/TheAboutSection.vue
@@ -0,0 +1,36 @@
+<template>
+	<section
+		class="container mx-auto py-32 text-black grid grid-cols-8 gap-x-8"
+	>
+		<h2
+			class="block sticky top-4 col-span-3 self-start font-primary font-bold text-3xl lg:text-5xl xl:text-6xl"
+		>
+			Poznaj nasze stowarzyszenie
+		</h2>
+		<div class="col-span-5">
+			<p class="opacity-95 text-xl max-w-prose">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+				Maiores aut ab alias iusto quibusdam. Distinctio eaque facilis
+				vitae cum fugiat, rem sit amet architecto sed obcaecati facere
+				animi est at delectus adipisci fugit dolores corrupti quisquam
+				velit ad, esse qui! Eos necessitatibus nemo laborum voluptate
+				hic, fugit culpa? Dicta delectus mollitia magnam eveniet.
+			</p>
+			<p class="opacity-95 text-xl max-w-prose mt-8">
+				Voluptates accusantium maiores harum, laudantium molestias
+				repudiandae assumenda quo molestiae praesentium cupiditate
+				nobis, veniam nemo, facere nihil alias voluptatibus dolores
+				vitae fuga exercitationem? Soluta atque nesciunt at autem vel
+				labore ex debitis aspernatur culpa corporis. Molestiae dolore at
+				ipsum vel culpa facilis totam temporibus quis iste voluptatem..
+			</p>
+		</div>
+	</section>
+	<div class="h-screen"></div>
+</template>
+<script>
+	export default {}
+</script>
+
+<style>
+</style>
